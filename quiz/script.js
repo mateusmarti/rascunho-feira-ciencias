0f5e163b458c776c9168c335814fdcd34c834949
KlGadioli <mateus.martins.rj@gmail.com>
2022-08-17 14:32:52 -0300

    Teste
    hjfhffdhjffvj

---
+++ quiz/script.js
@@ -0,0 +1,12 @@
+function valuetipo(){
+    //entrada
+    let tipo = document.form.res.value;
+
+    //processamento
+    if(tipo == "2"){
+        alert("Resposta Correta");
+    }
+    else {
+        alert("Errado");
+    }
+}
